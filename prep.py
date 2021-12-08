96ba03a55f26bc8d09afa90ffd0c4e07cf7fc2e5
Thanh Son Phung <tphung@student13.cse.nd.edu>
2021-12-08 13:56:24 -0500

    adds preparation script

---
+++ prep.py
@@ -0,0 +1,13 @@
+#import relevant libraries
+import os
+
+#get desired number of nodes
+num_nodes = int(input('> '))
+
+#clone as many nodes
+for i in range(num_nodes):
+    os.system(f'git clone https://github.com/tphung3/blockchain.git node{i}')
+
+#generate pairs of keys for all nodes EXCEPT FIRST NODE as first node needs to claim the mining of the genesis block
+for i in range(1, num_nodes):
+    os.system(f'cd node{i};./gen-keys.py -F')
